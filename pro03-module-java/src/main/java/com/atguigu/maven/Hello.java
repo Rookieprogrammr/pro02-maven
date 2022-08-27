8b31fee5788d30ec463e357b439521f5d8bccac1
Anton <3148144329@qq.com>
2022-08-27 21:42:24 +0800

    second commit

--- pro03-module-java/src/main/java/com/atguigu/maven/Hello.java
+++ pro03-module-java/src/main/java/com/atguigu/maven/Hello.java
@@ -3,5 +3,6 @@ package com.atguigu.maven;
 public class Hello {
     public void showMessage(){
         System.out.println("Hello maven!use idea!");
+        System.out.println("Hello again!");
     }
 }
